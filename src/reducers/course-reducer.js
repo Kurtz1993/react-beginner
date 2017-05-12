6032fbae97555dab3fc4a36f0bce3d5b84661205
Luis Rogelio Hernández López <lhernandez@bofifederalbank.com>
2017-05-11 20:49:25 -0500

    Fix action typo

--- src/reducers/course-reducer.js
+++ src/reducers/course-reducer.js
@@ -1,6 +1,6 @@
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case 'CREATE_COUSRE':
+        case 'CREATE_COURSE':
             return [...state,
                 Object.assign({}, action.course)
             ];
