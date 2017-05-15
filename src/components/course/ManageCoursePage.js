8cf6fbe65f7cd5eb356e0755a825bd2e567459de
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-05-14 21:46:13 -0500

    Add validation for title

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -31,9 +31,26 @@ export class ManageCoursePage extends React.Component {
     return this.setState({ course });
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({ errors });
+    return formIsValid;
+  }
+
   saveCourse(event) {
-    this.setState({ isSaving: true });
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
+    this.setState({ isSaving: true });
     this.props
       .actions
       .saveCourse(this.state.course)
