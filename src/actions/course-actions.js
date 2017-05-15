92618ecfd44c5ddbf53f008003c961ae9d46d85e
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-05-14 20:19:43 -0500

    Update thunks to dispatch beginAjaxCall

--- src/actions/course-actions.js
+++ src/actions/course-actions.js
@@ -1,5 +1,6 @@
 import * as types from './action-types';
 import courseApi from '../api/mock-course-api';
+import { beginAjaxCall } from './ajax-status-actions';
 
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -15,6 +16,7 @@ export function createCourseSuccess(course) {
 
 export function loadCourses() {
     return function (dispatch) {
+        dispatch(beginAjaxCall());
         return courseApi
             .getAllCourses()
             .then(courses => {
@@ -28,6 +30,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
     return (dispatch, getState) => {
+        dispatch(beginAjaxCall());
         return courseApi.saveCourse(course)
             .then(savedCourse => {
                 course.id ?
