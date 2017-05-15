0bc64da04bde4fd09ad3f76591835d019a9aac11
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-05-14 19:56:49 -0500

    Populate from when clicking existing course

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -57,8 +57,19 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(c => c.id === id);
+  if (course) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
   let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
